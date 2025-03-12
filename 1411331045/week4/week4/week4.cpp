0adaa43239a38a9b1d78bc6b420fc0f4b5d2b53b
1411331045 <bobo368587@gmail.com>
2025-03-12 12:04:48 +0800

    Add files via upload

---
+++ 1411331045/week4/week4/week4.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream> 
+using namespace std;
+int volume(int);
+double volume(int, int);
+int volume(int, int, int);
+int a;int b; int c;
+int main()
+{
+	double sideValue = 10.0;
+	float result;
+	cout << "請輸入一個整數" << endl;
+	cin >> a;
+	cout << volume(a) << endl;
+	cout << "請輸入兩個整數" << endl;
+	cin >> a >> b;
+	cout << volume(a, b) << endl;
+	cout << "請輸入三個整數" << endl;
+	cin >> a >> b >> c;
+	cout << volume(a, b, c) << endl;
+	cout << "finished!" << endl;
+}
+
+
+int volume(int a)
+{
+	return a * a * a;
+}
+
+double volume(int a ,int b)
+{
+	return a * a * 3.14159 * b;
+}
+
+int volume(int a,int b ,int c)
+{
+	return a * b * c;
+}
+
