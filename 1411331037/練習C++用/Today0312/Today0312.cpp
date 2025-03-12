6d54407eba44d7fa02bdcbaa9b9abff6b562b10f
s1411331037 <148743796+YehYuLu@users.noreply.github.com>
2025-03-12 12:01:33 +0800

    Add files via upload

--- 1411331037/練習C++用/Today0312/Today0312.cpp
+++ 1411331037/練習C++用/Today0312/Today0312.cpp
@@ -1,26 +1,35 @@
-﻿// Exercise 18.4 Solution: Ex18_04.cpp
-// Inline function that calculates the volume of a sphere.
+﻿// Fig. 15.5: fig15_05.cpp
+// Comparing pass-by-value and pass-by-reference with references.
 #include <iostream>
-#include <cmath>
 using namespace std;
 
-const double PI = 3.14159; // define global constant PI
+int x, y, z;
+int area(int);
+double area(int, int);
+double area(int, int, int);
 
-// calculates volume of a sphere
-inline double sphereVolume(const double radius)
-{
-    return 4.0 / 3.0 * PI * pow(radius, 3);
+
+int main() {
+	cout << "輸入一個正數" << endl;
+	cin >> x;
+	cout << "正方體面積:" << area(x);
+	cout << "\n輸入二個正數" << endl;
+	cin >> x >> y;
+	cout << "圓柱體面積:" << area(x, y);
+	cout << "\n輸入三個正數" << endl;
+	cin >> x >> y >> z;
+	cout << "長方體面積:" << area(x, y, z);
+
+	cout << "\n結束" << endl;
 }
 
-int main()
-{
-    double radiusValue;
+int area(int x) {
+	return x * x * x;
+}
 
-    // prompt user for radius 
-    cout << "輸入你的半徑長度: ";
-    cin >> radiusValue; // input radius 
-
-    // use radiusValue to calculate volume of sphere and display result
-    cout << "半徑為 " << radiusValue
-        << "的面積是" << sphereVolume(radiusValue) << endl;
+double area(int x, int y) {
+	return x * x * 3.1415926 * y;
+}
+double area(int x, int y, int z) {
+	return x * y * z;
 }
