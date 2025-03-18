0915dc01ca5612e333b2f8027901d0f824f4402a
s1411331025 <jk070591@gmail.com>
2025-03-18 23:22:15 +0800

    Add files via upload

---
+++ 1411331025/week1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,36 @@
+﻿
+
+#include <cstdio>
+#include <cstddef>
+
+// clientData structure definition       
+
+
+struct studentData {   //定義結構
+
+    int     ID;
+    char    name[20];
+    int     math, comput;
+    float   AVG;
+}stu;
+
+int main(void)
+{
+    FILE* cfPtr = NULL; // accounts.dat file pointer
+
+    // fopen opens the file; exits if file cannot be opened
+    if ((fopen_s(&cfPtr, "accounts.dat", "wb")) != 0) {
+        puts("File could not be opened.");
+    }
+    else {
+        // create clientData with default information       
+        struct studentData blankstudent = { 0, "", 0 , 0.0 };
+
+        // output 100 blank records to file                              
+        for (unsigned int i = 1; i <= 100; ++i) {
+            fwrite(&blankstudent, sizeof(struct studentData), 1, cfPtr);
+        }
+
+        fclose(cfPtr); // fclose closes the file
+    }
+}
---
+++ 1411331025/week2/week2.cpp
@@ -0,0 +1,62 @@
+﻿// Creating a random-access file sequentially
+#include <stdio.h>
+
+// clientData structure definition       
+
+
+struct studentData {   //定義結構
+
+    int     ID;
+    char    name[20];
+    int     math, comput;
+    float   AVG;
+}stu;
+;
+int main(void)
+{
+    FILE* cfPtr = NULL; // accounts.dat file pointer
+    typedef struct suudentData stu;
+    // fopen opens the file; exits if file cannot be opened
+    if ((fopen_s(&cfPtr, "accounts.dat", "rb+")) != 0) {
+        puts("File could not be opened.");
+    }
+    else {
+        // create clientData with default information       
+        struct studentData blankstudent = { 0, "", 0 ,0, 0.0 };
+
+        // output 100 blank records to file                              
+        for (unsigned int i = 1; i <= 100; ++i) {
+            fwrite(&blankstudent, sizeof(struct studentData), 1, cfPtr);
+        }
+
+        // require user to specify account number
+        printf("%s", "Enter account ID"
+            " (1 to 100, 0 to end input): ");
+        scanf_s("%d", &blankstudent.ID);
+
+        // user enters information, which is copied into file
+        while (blankstudent.ID != 0) {
+            // user enters last name, first name and balance
+            printf("%s", "name,math,comput: ");
+
+            // set record lastName, firstName and balance value
+            scanf_s("%14s%9d%d", blankstudent.name, 14, &blankstudent.math,
+                &blankstudent.comput);
+            blankstudent.AVG = (blankstudent.math + blankstudent.comput) / 2;
+
+
+            // seek position in file to user-specified record   
+            fseek(cfPtr, (blankstudent.ID - 1) *
+                sizeof(struct studentData), SEEK_SET);
+
+            // write user-specified information in file              
+            fwrite(&blankstudent, sizeof(struct studentData), 1, cfPtr);
+
+            // enable user to input another account number
+            printf("%s", "Enter account number: ");
+            scanf_s("%d", &blankstudent.ID);
+        }
+
+        fclose(cfPtr); // fclose closes the file
+    }
+}
