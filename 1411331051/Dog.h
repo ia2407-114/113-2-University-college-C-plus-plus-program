d31a04e79e52bb68e72692d0a476feb3a95bc06e
1411331051 <eric123kwok@gmail.com>
2025-04-02 11:48:28 +0800

    Add files via upload

---
+++ 1411331051/Dog.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <string>
+using namespace std;
+
+class Dog {
+private:
+    string name;
+    int age;
+
+public:
+    // Constructor to initialize dog's name and age
+    Dog(string n, int a);
+
+    // Set the dog's name
+    void setName(string n);
+
+    // Get the dog's name
+    string getName() const;
+
+    // Set the dog's age with validation
+    void setAge(int a);
+
+    // Get the dog's age
+    int getAge() const;
+
+    // Print the dog's information
+    void printInfo() const;
+};
