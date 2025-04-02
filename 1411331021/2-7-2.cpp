c5e1d2ea0ad8c9ddf68f031ac98de9ee9f8b2d91
chiao0o <h0903656337@gmail.com>
2025-04-02 11:53:44 +0800

    2-7

---
+++ 1411331021/2-7-2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string> 
+#include "Dog.h"
+using namespace std;
+
+int main()
+{
+    string nameOfDog;
+    int age;
+    Dog myDog;
+    cout << "Initial dog's name is: " << myDog.getDogName() << endl;
+    cout << "\nInitial dog's age is: " << myDog.getDogAge() << endl;
+
+    cout << "\nPlease enter the dog's name and age:" << endl;
+    getline(cin, nameOfDog);
+    cin >> age;
+    myDog.setDogName_and_Age(nameOfDog, age);
+    cout << endl;
+    myDog.displayMessage();
+}
+
+
