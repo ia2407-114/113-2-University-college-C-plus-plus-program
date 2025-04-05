f5e13907b113818b15eae2c1c32d432609ba8c9a
1411331011 <chelseapeng060505@gmail.com>
2025-04-05 22:29:40 +0800

    Update dog.cpp

--- 1411331011/0402-2/dog.cpp
+++ 1411331011/0402-2/dog.cpp
@@ -1,1 +1,47 @@
+#include <iostream>
+#include <string> // program uses C++ standard string class
+#include"dog.h"
+using namespace std;
+
+// Dog class definition
+DOG:: DOG(string name, int age)
+    {
+        DogName = name;
+        DogAge = age;
+    }
+    // function that sets the course name
+void DOG::setDogName_and_Age(string name, int age)
+{
+    if (name.size() <= 10) // if name has 25 or fewer characters
+        DogName = name;
+        DogAge = age;
+
+    if (name.size() > 10)
+    {
+       
+        DogName = name.substr(0, 10);
+
+        cerr << "Name \"" << name << "\" exceeds maximum length (10).\n"
+            << "Limiting courseName to first 10 characters.\n" << endl;
+    } // end if
+} // end function setCourseName
+
+ string DOG::getDogName() const
+    {
+        return DogName; // return the object's courseName
+    } // end function getCourseName
+
+ int DOG::getDogAge() const
+    {
+        return DogAge; // return the object's courseName
+    } // end function getCourseName
+
+    // function that displays a welcome message
+ void DOG::displayMessage() const
+    {
+        // this statement calls getCourseName to get the
+        // name of the course this GradeBook represents
+        cout << "This my dog named %s\n" << getDogName() << "!" << endl;
+        cout << "This my dog now is age %d\n" << getDogName() << "!" << endl;
+    } // end function displayMessage
 
