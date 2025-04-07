187192ae11d5c32893bcc00b13d9c6a75d62b81a
s1411331023 <tina08040816@gmail.com>
2025-04-08 00:04:34 +0800

    Create dog.h

---
+++ 1411331023/week7-1/dog.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <iostream>
+#include <string> // program uses C++ standard string class
+using namespace std;
+
+
+class DOG
+{
+public:
+    explicit  DOG(string name, int age);
+
+    // function that sets the course name
+    void setDogName_and_Age(string name, int age);
+
+    // function that gets the course name
+    string getDogName() const;
+   
+    int getDogAge() const;
+
+    // function that displays a welcome message
+    void displayMessage() const;
+
+private:
+    string DogName; // course name for this GradeBook
+    int DogAge;
+}; // end class GradeBook  
+
