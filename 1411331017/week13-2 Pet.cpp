ccc012d22a1443ceb1939d8ca02850059a6c7a0e
s1411331017 <a0905602675@icloud.com>
2025-05-14 22:08:04 +0800

    Add files via upload

---
+++ 1411331017/week13-2 Pet.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "week13-2 Pet.h"
+#include "week13-2 Date.h"
+using namespace std;
+
+Pet::Pet(const string& first, const string& last, const Date& dateOfBirth, const Date& dateOfAdopt)
+	: firstName(first), lastName(last), birthDate(dateOfBirth), adoptDate(dateOfAdopt) {
+	cout << "Pet object constructor: " << this->firstName << ' ' << this->lastName << endl;
+}
+
+Pet& Pet::printFirstName() {
+	cout << "First name: " << this->firstName << endl;
+	return *this;
+}
+
+Pet& Pet::printLastName() {
+	cout << "Last name: " << this->lastName << endl;
+	return *this;
+}
+
+Pet& Pet::printDates() {
+	cout << "Adoption Date: ";
+	this->adoptDate.print();
+	cout << ", Birth Date: ";
+	this->birthDate.print();
+	cout << endl;
+	return *this;
+}
+
+void Pet::print() const {
+	cout << lastName << ", " << firstName << "  Adopted: ";
+	adoptDate.print();
+	cout << "  Birthday: ";
+	birthDate.print();
+	cout << endl;
+}
+
+Pet::~Pet() {
+	cout << "Pet object destructor: " << this->lastName << ", " << this->firstName << endl;
+}
---
+++ 1411331017/week13-2 Pet.h
@@ -0,0 +1,23 @@
+#pragma once
+#ifndef PET_H
+#define PET_H
+
+#include <string>
+#include "week13-2 Date.h"
+
+class Pet {
+public:
+	Pet(const std::string&, const std::string&, const Date&, const Date&);
+	void print() const;
+	Pet& printFirstName();
+	Pet& printLastName();
+	Pet& printDates();
+	~Pet();
+private:
+	std::string firstName;
+	std::string lastName;
+	const Date birthDate;
+	const Date adoptDate;
+};
+
+#endif
