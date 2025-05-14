85ca59f84e8e280dc603761d7c3c50b3bf5e347f
s1411331023 <tina08040816@gmail.com>
2025-05-14 11:47:53 +0800

    Create main.cpp

---
+++ 1411331023/week13/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "Pet.h"
+using namespace std;
+
+int main() {
+	string firstName, lastName;
+	int birthMonth, birthDay, birthYear;
+	int adoptMonth, adoptDay, adoptYear;
+
+	cout << "Enter pet's first name: ";
+	cin >> firstName;
+	cout << "Enter pet's last name: ";
+	cin >> lastName;
+
+	cout << "Enter pet's birth date (MM DD YYYY): ";
+	cin >> birthMonth >> birthDay >> birthYear;
+	Date birth(birthMonth, birthDay, birthYear);
+
+	cout << "Enter pet's adoption date (MM DD YYYY): ";
+	cin >> adoptMonth >> adoptDay >> adoptYear;
+	Date adopt(adoptMonth, adoptDay, adoptYear);
+
+	Pet pet(firstName, lastName, birth, adopt);
+	cout << endl;
+	pet.printFirstName().printLastName().printDates();
+}
