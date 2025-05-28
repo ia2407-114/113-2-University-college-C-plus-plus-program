f0863fa7c379b3eee4277ef6f5d12e49c3c12916
s1411331031 <a0958254876@icloud.com>
2025-05-28 11:30:00 +0800

    Add files via upload

---
+++ 1411331031/20250528-1/Source.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+#include "Matrix.h"
+int main()
+{
+    int i, j, row, column;
+    cout << "重新定義「*=、+=、-=」運算子多載，使其能應用在矩陣上\n";
+    cout << "輸入矩陣A及B的列數(row):";
+    cin >> row;
+    cout << "輸入矩陣A及B的行數(column):";
+    cin >> column;
+    Matrix a('A', row, column);
+    Matrix b('B', row, column);
+    Matrix c(row, column);
+    cout << "---------------" << endl;
+    cout << "矩陣A= \n";
+    for (i = 0;i < row;i++)
+    {
+        for (j = 0;j < column;j++)
+            cout << a.element[i][j] << '\t';
+        cout << endl;
+    }
+    cout << endl;
+    cout << "矩陣B = \n";
+    for (i = 0;i < row;i++)
+    {
+        for (j = 0;j < column;j++)
+            cout << b.element[i][j] << '\t';
+        cout << endl;
+    }
+    cout <<"---------------" << endl;
+    c = a *= b;  // 矩陣A * 矩陣B
+    cout << "矩陣A * 矩陣B = \n";
+    for (i = 0;i < row;i++)
+    {
+        for (j = 0;j < column;j++)
+            cout << c.element[i][j] << '\t';
+        cout << endl;
+    }
+    cout << "---------------" << endl;
+    c = a += b;  // 矩陣A + 矩陣B
+
+    cout << "矩陣A + 矩陣B = \n";
+    for (i = 0;i < row;i++)
+    {
+        for (j = 0;j < column;j++)
+            cout << c.element[i][j] << '\t';
+        cout << endl;
+    }
+    cout << "---------------" << endl;
+    c = a -= b;  // 矩陣A - 矩陣B
+
+    cout << "矩陣A - 矩陣B = \n";
+    for (i = 0;i < row;i++)
+    {
+        for (j = 0;j < column;j++)
+            cout << c.element[i][j] << '\t';
+        cout << endl;
+    }
+    cout << "---------------" << endl;
+    return 0;
+}
