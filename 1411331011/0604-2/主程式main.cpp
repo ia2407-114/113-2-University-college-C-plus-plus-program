819fccc30f68a690271457e9c282b3bf00eb1117
1411331011 <chelseapeng060505@gmail.com>
2025-06-04 12:01:55 +0800

    Create 主程式main.cpp

---
+++ 1411331011/0604-2/主程式main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "square.h"
+using namespace std;
+
+int main() {
+    
+    square_2D rect2D;
+    rect2D.print_2D();
+
+    cout << endl;
+
+    
+    rect_3D rect3D;
+    rect3D.print_3D();
+
+    return 0;
+}
