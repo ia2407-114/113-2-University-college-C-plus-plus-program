aad7c08d7341a17a9d062d2f4ca9108a1882ea41
s1411205001 <jean1835521@gmail.com>
2025-06-11 10:35:52 +0800

    Add files via upload

---
+++ 1411205001/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include "air.h"
+using namespace std;
+
+int main()
+{
+	airliner air1;  // 兼具空中及水上功能   
+	air1.flight_object::display();
+	air1.display();
+
+	return 0;
+}
